88ad0e110b94790a695530508a4b61c8542a8653
jcrousse <jcrousse@gmail.com>
2020-09-04 18:43:31 +0200

    cleanup

--- nn_model/nn_pattern_identification.py
+++ nn_model/nn_pattern_identification.py
@@ -3,12 +3,7 @@ from data_sources.data_generator import ExamplesGenerator
 
 
 # TODO:
-#  -Adjust seq mining to find sequences that are over-represented in on label.
-#       -Remove sub-sequences that are not higher in predictive power.
-#       -One single var model per sequence? Groups of sequences?
-#  -How to simulate sub sequences that are positive or negative within the pattern? Is it necessary?
-#       Or just mention it?
-#  -More realistic: More than one possible pattern?
+#  -Recover sequences with prefixspan
 
 def get_dataset(seq_len, vocab_size, seed, pattern=None, batch_size=200, multiple_patterns=None, **kwargs):
     data_generator = ExamplesGenerator(seq_len=seq_len, vocab_size=vocab_size, seed=seed,
@@ -67,7 +62,7 @@ def run_experiment(name, seq_len, vocab_size, pattern=None, data_limit=None, mul
               epochs=40,
               steps_per_epoch=50,
               validation_steps=1,
-              callbacks=[tensorboard])
+              callbacks=[tensorboard, early_stop])
     model.evaluate(test_dataset.take(1000), verbose=2)
 
 
@@ -84,7 +79,6 @@ def get_multiple_patterns(n=2):
 
 if __name__ == '__main__':
     tokens1 = [10, 11, 12, 13, 14]
-
 
     experiments = {
         "alpha": {
